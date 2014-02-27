36dbc54acb75000dea7d84a5f8f3d94053017424
korbax <korbax@ubuntu-vrb>
2014-02-27 17:51:09 +0200

    Task490 clear etc and sql

--- app/code/local/Ct/SetBackground/sql/setbackground_setup/mysql4-install-0.1.1.php
+++ app/code/local/Ct/SetBackground/sql/setbackground_setup/mysql4-install-0.1.1.php
@@ -1,7 +1,23 @@
 <?php
+/**
+* NOTICE OF LICENSE
+*
+* You may not sell, sub-license, rent or lease
+* any portion of the Software or Documentation to anyone.
+*
+* DISCLAIMER
+*
+* Do not edit or add to this file if you wish to upgrade to newer
+* versions in the future.
+*
+* @category   Ct
+* @package    Ct_SetBackground
+* @copyright  Copyright (c) 2014 Ct Web Solutions (http://codetiburon.com/)
+* @contacts   info@codetiburon.com
+* @license    http://shop.etwebsolutions.com/etws-license-free-v1/   ETWS Free License (EFL1)
+*/
 
 $installer = $this;
-
 $installer->startSetup();
 
 $installer->run("
